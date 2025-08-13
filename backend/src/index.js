beee9003e14523ab8165b1c3020b0a2353a92b98
Aditya Chavan <chavanaditya9325@gmail.com>
2025-08-13 15:45:22 +0530

    initial fixes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './db/index.js';
+import {app} from './app.js'
 
 dotenv.config({
     path: './env'
