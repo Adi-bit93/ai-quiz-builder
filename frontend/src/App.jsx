ab6c59133779a4b6b977fda72029a7c4f2f5a263
Aditya Chavan <chavanaditya9325@gmail.com>
2025-08-20 17:57:21 +0530

    add login page and route for user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />}/>
+          <Route path="/login" element={<Login/>}></Route>
         </Routes>
       </BrowserRouter>
     </AuthProvider>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useNavigate ,Link } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.jsx";
+import  { apiRequest } from "../lib/api.js";
+
+export default function Login(){
+    const [form, setform] = useState({email: "", password: ""});
+    const { login } = useAuth();
+    const nav = useNavigate();
+
+    const handlechange = (e) => setform({...form, [e.target.name]: e.target.value});
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const res = await apiRequest("/api/auth/login", {method: "POST", body: form});
+
+        const json = await res.json().catch(() => ({}));
+        if(res.ok){
+            login(json.data.accessToken, json.data.user);
+            nav("/dashboard");
+        }else {
+            alert(json?.message || "Login failed");
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-300">
+            <form onSubmit={handleSubmit} className=" bg-white p-8 rounded-2xl shadow-sm w-full max-w-md">
+                <h1 className="text-2xl font-bold mb-6">Organizer login</h1>
+                <input className="border p-3 rounded w-full mb-3" name="email" placeholder="Email" type="email" onChange={handlechange} />
+                <input className="border p-3 rounded w-full mb-6" name="password" placeholder="Password" type="password" onChange={handlechange} />
+                <button className="w-full bg-green-600 text-white py-3 rounded-lg">Submit</button>
+                <p className=" text-sm mt-4">New here? <Link to="/signup" className="text-blue-700 hover:underline">Crete account</Link></p>
+            </form>
+        </div>
+    )
+}
