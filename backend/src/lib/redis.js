0cba6d3789fb7efcb67c82059f7e52795e80de5c
Aditya Chavan <chavanaditya9325@gmail.com>
2025-10-24 18:45:03 +0530

    adding redis database server with upstash

--- backend/src/lib/redis.js
+++ backend/src/lib/redis.js
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => console.log("✅ Connected to Redis"));
 redis.on("error", (err) => console.error("❌ Redis Error:", err));
